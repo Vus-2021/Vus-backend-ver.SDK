7cf751b3b6e3baa70ed559cfb48a5c666c8fbcbf
younghoon.choi <c_yh0327@naver.com>
2021-04-02 15:14:15 +0900

    [feat] create notice

---
+++ src/services/notice/createNotice.js
@@ -0,0 +1,20 @@
+const AWS = require('aws-sdk');
+const documentClient = new AWS.DynamoDB.DocumentClient();
+
+const createNotice = async (args) => {
+    const params = {
+        TableName: process.env.TABLE_NAME,
+        Item: {
+            ...args,
+        },
+    };
+    try {
+        await documentClient.put(params).promise();
+        return { success: true, message: 'success crete notice', code: 201 };
+    } catch (error) {
+        console.log(error);
+        return { success: false, message: error.message, code: 500 };
+    }
+};
+
+module.exports = createNotice;
